40712feed4f32a2098c6ba375e5b1e32c6f751f2
Rodrigo Junqueira <eurodrigojunqueira@gmail.com>
2024-09-02 17:22:18 -0300

    Evento Pt1

---
+++ script.js
@@ -0,0 +1,25 @@
+const img = document.querySelector("img");
+function callback(event) {
+  console.log(event);
+}
+
+img.addEventListener("click", callback);
+
+const animaisLista = document.querySelector(".animais-lista");
+function callbackLista(event) {
+  console.log(event.currentTarget); // this
+  console.log(event.target); // onde o clique ocorreu
+  console.log(event.type); // tipo de evento
+}
+
+animaisLista.addEventListener("click", callbackLista);
+
+console.log(animaisLista);
+
+const linkExterno = document.querySelector('a[href^="http"]');
+function handleLinkExterno(event) {
+  event.preventDefault;
+  console.log(this);
+}
+
+linkExterno.addEventListener("click", handleLinkExterno);
