40a4e9e2e57995e6f98fa20be5f4a76a1e49abd9
Rodrigo Junqueira <eurodrigojunqueira@gmail.com>
2024-09-05 17:12:29 -0300

    Javascript final: Navegação por Tabs

--- script.js
+++ script.js
@@ -1,53 +1,22 @@
-// const h1 = document.querySelector("h1");
-// const animaisLista = document.querySelector(".animais-descricao");
+// Selecionar os itens
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-// // h1.innerHTML = "<p>Novo Título</p>";
+if (tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add("ativo"); // Deixar sem pre o primeiro item ativo
 
-// // console.log(animaisLista.innerHTML);
+  // Função Callback - Recebe index como parâmetro. Sempre que ativar, remove a classe ativo de todos os outros elementos
+  function activeTab(index) {
+    tabContent.forEach((content) => {
+      content.classList.remove("ativo");
+    });
+    tabContent[index].classList.add("ativo");
+  }
 
-// const lista = document.querySelector(".animais-lista");
-
-// console.log(lista.children);
-
-// const animais = document.querySelector(".animais");
-// const contato = document.querySelector(".contato");
-// const titulo = contato.querySelector(".titulo");
-
-// // animais.appendChild(contato);
-
-// const novoH1 = document.createElement("h1");
-// novoH1.innerText = "Novo Título usando o método createElement";
-// novoH1.classList.add("titulo");
-
-// contato.appendChild(novoH1);
-
-// const h1 = document.querySelector("h1");
-// const faq = document.querySelector(".faq");
-
-// const cloneH1 = h1.cloneNode(true);
-
-// faq.appendChild(cloneH1);
-
-// Exercícios:
-
-// 1. Duplique o menu e adicione ele em copy
-const menu = document.querySelector(".menu");
-const copy = document.querySelector(".copy");
-
-const cloneMenu = menu.cloneNode(true);
-copy.appendChild(cloneMenu);
-
-// 2. Selecione o primeiro DT da dl de Faq
-const faq = document.querySelector(".faq");
-const primeiroDt = faq.querySelector("dt");
-
-console.log(primeiroDt);
-
-// 3. Selecione o DD referente ao primeiro DT
-const proximoDD = primeiroDt.nextElementSibling;
-console.log(proximoDD);
-
-// 4. Substitua o conteúdo html de .faq pelo de .animais
-const animais = document.querySelector(".animais");
-
-faq.innerHTML = animais.innerHTML;
+  // Adicionar o evento de click
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener("click", () => {
+      activeTab(index);
+    });
+  });
+}
