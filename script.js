1bc939d667730dc45e10eee7384d4de4ba3d3077
Rodrigo Junqueira <eurodrigojunqueira@gmail.com>
2024-09-12 15:41:07 -0300

    Accordion List on FAQ

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-// Selecionar os itens
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+function initTabNav() {
+  // Selecionar os itens
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-if (tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add("ativo"); // Deixar sem pre o primeiro item ativo
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo"); // Deixar sempre o primeiro item ativo
 
-  // Função Callback - Recebe index como parâmetro. Sempre que ativar, remove a classe ativo de todos os outros elementos
-  function activeTab(index) {
-    tabContent.forEach((content) => {
-      content.classList.remove("ativo");
+    // Função Callback - Recebe index como parâmetro. Sempre que ativar, remove a classe ativo de todos os outros elementos
+    function activeTab(index) {
+      tabContent.forEach((content) => {
+        content.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
+
+    // Adicionar o evento de click
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
     });
-    tabContent[index].classList.add("ativo");
   }
-
-  // Adicionar o evento de click
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener("click", () => {
-      activeTab(index);
-    });
-  });
 }
+
+initTabNav();
+
+function initAccordion() {
+  // Selecionar o dt do FAQ
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo";
+
+  if (accordionList.length) {
+    // Verificar se existe item de lista
+    // Deixar sempre o primeiro item ativo
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    // Função para adicionar o ativo ao clique
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+
+initAccordion();
